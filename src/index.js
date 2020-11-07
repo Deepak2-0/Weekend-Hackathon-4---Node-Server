51a5f5b72687d2249f2068619f66d7a9a816f4c3
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-07 17:54:52 +0530

    trail 1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  if(req.url==="/welcome"){
+    res.status(200).write("Welcome to Dominos!");
+    res.end();
+  }
+
+  else if(req.url === "/contact"){
+    res.status(200).write("{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}");
+  }
+
+  else{
+    res.status(404).write(" 404 not found");
+  }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
