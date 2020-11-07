c0ee9c290cb4c4f3221b91186a34b4098a36f2a2
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-07 18:44:50 +0530

    trail 3

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   if(req.url==="/welcome"){
-    res.writeHead(200,"Welcome to Dominos!");
+    //res.writeHead(200,"Welcome to Dominos!");
+	res.writeHead(200);
 	res.write("Welcome to Dominos!");
     res.end();
   }
 
   else if(req.url === "/contact"){
-    res.writeHead(200,"{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}");
+    //res.writeHead(200,"{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}");
+	res.writeHead(200);
 	res.write("{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}");
     res.end();
   }
